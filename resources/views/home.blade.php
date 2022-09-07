735f2025d68dc1bc72e82e082e0c7e3e0d29420e
Antonius131 <antomendetta@gmail.com>
2022-09-07 15:54:45 +0200

    changing home layout

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,14 +1,24 @@
 @extends('layouts.main ')
 
 @section('main-cards')
-<div class="card">
-   <div class="comic-front">
-      <img src="#" alt="comicSeries">
+<div class="container">
+   <div class="comic-title">
+     <h3>Current series</h3>
    </div>
-   <div class="comic-caption">
-      <h4>
-         Comic Series
-      </h4>
+   <div id="comics-list">
+      <div class="card">
+         <div class="comic-front">
+            <img src="#" alt="comicSeries">
+         </div>
+         <div class="comic-caption">
+            <h4>
+               Comic Series
+            </h4>
+         </div>
+      </div>
    </div>
-</div>
+   <div class="content-options">
+     <button type="button">Load more</button>
+   </div>
+ </div>
 @endsection
