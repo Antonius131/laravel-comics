5bf162faf88a7ae042cbf6fbae9d5c35584d81d1
Antonius131 <antomendetta@gmail.com>
2022-09-07 15:33:20 +0200

    creating cards layout

---
+++ resources/views/includes/cards.blade.php
@@ -0,0 +1,10 @@
+<div class="card">
+   <div class="comic-front">
+      <img src="#" alt="comicSeries">
+   </div>
+   <div class="comic-caption">
+      <h4>
+         Comic Series
+      </h4>
+   </div>
+</div>
