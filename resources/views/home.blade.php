eb905e572003d15c43d22ac5821c9b55a2f20cf6
Antonius131 <antomendetta@gmail.com>
2022-09-07 16:53:38 +0200

    dynamically create comics list

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -6,16 +6,18 @@
      <h3>Current series</h3>
    </div>
    <div id="comics-list">
-      <div class="card">
-         <div class="comic-front">
-            <img src="#" alt="comicSeries">
+      @foreach ($comics as $comic)
+         <div class="card">
+            <div class="comic-front">
+               <img src="{{ $comic->image }}" alt="{{ $comic->type }}">
+            </div>
+            <div class="comic-caption">
+               <h4>
+                  {{ $comic->title }}
+               </h4>
+            </div>
          </div>
-         <div class="comic-caption">
-            <h4>
-               Comic Series
-            </h4>
-         </div>
-      </div>
+      @endforeach
    </div>
    <div class="content-options">
      <button type="button">Load more</button>
